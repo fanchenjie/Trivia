05efd8182fad954faf3f28d2f58863fca3d8f533
Chenjie Fan <fanchenjie@seattleu.edu>
2020-10-23 22:14:54 -0700

    set up CORS  @app.after_request   connect to db

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
@@ -12,20 +12,36 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  cors = CORS(app, resources={r"/*":{"origins":"*"}})
+  setup_db(app)
+  
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers','Content-Type, Authorization, true')
+    response.headers.add('Access-Control-Allow-Methods','GET,PATCH,POST,DELETE,OPTIONS')
+    return response
+
+  @app.route('/')
+  def index():
+    return jsonify({'message':'hello'})
+
+
+
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  # @app.route('/categories')
+  # def get_categories():
+  #   db.query.all()
 
 
   '''
